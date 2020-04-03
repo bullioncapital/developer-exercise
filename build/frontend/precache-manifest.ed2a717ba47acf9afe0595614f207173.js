3f173926e74cfc46b9102e24dd5d43b756faf6ea
Matías Medrano Gamonal <matiasmedrano94@gmail.com>
2020-04-03 15:15:38 +1000

    Frontend production build ready

---
+++ build/frontend/precache-manifest.ed2a717ba47acf9afe0595614f207173.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "490a9cc5ad83155cd9be7c20c4e4812a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4f18cc9adcc658b8603",
+    "url": "/static/css/main.94e63586.chunk.css"
+  },
+  {
+    "revision": "f9cd74d133a92b034af0",
+    "url": "/static/js/2.3a79af6f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3a79af6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4f18cc9adcc658b8603",
+    "url": "/static/js/main.70790b8b.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
