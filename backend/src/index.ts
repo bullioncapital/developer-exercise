bb2263fa043f68e69fa432da404cc411f9353352
Myles Wardell <myles.wardell212@gmail.com>
2020-06-30 16:30:14 +1000

    Basic Pagination

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,126 @@
 import express from "express";
-import * as sqlite3 from "sqlite3";
-import * as cors from 'cors'
+import sqlite3 from "sqlite3";
+import cors from 'cors'
 
-const PORT = 3000
+const PORT = 3001
 const db = new sqlite3.Database("db");
 const app = express();
-
 app.use(cors());
-app.get("/", function (req, res) {
-  db.all("SELECT * FROM farmers_markets_from_usda", (error, data) => {
-    if (error) {
-      console.log(JSON.stringify(error, null, 2))
-      res.sendStatus(500)
-    } else {
-      res.send(data);
-    }
-  });
+
+app.get("/FarmersMarket", async function (req, res) {
+  try {
+    const pagination: number = Math.round((req?.query?.pagination || 0) as number)
+    const limit: number = (req?.query?.limit || 20) as number
+    const farmersMarketData = await getFarmersMarketDataCache(pagination, limit)
+    res.send(farmersMarketData)
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(500)
+  }
+  
 });
+
+interface FarmersMarketData {
+  FMID: any,
+  MarketName: any,
+  Website: any,
+  Facebook: any,
+  Twitter: any,
+  Youtube: any,
+  OtherMedia: any,
+  street: any,
+  County: any,
+  State: any,
+  zip: any,
+  Season1Date: any,
+  Season1Time: any,
+  Season2Date: any,
+  Season2Time: any,
+  Season3Date: any,
+  Season3Time: any,
+  Season4Date: any,
+  Season4Time: any,
+  x: any,
+  y: any,
+  Location: any,
+  Credit: any,
+  WIC: any,
+  WICcash: any,
+  SFMNP: any,
+  SNAP: any,
+  Organic: any,
+  Bakedgoods: any,
+  Cheese: any,
+  Crafts: any,
+  Flowers: any,
+  Eggs: any,
+  Seafood: any,
+  Herbs: any,
+  Vegetables: any,
+  Honey: any,
+  Jams: any,
+  Maple: any,
+  Meat: any,
+  Nursery: any,
+  Nuts: any,
+  Plants: any,
+  Poultry: any,
+  Prepared: any,
+  Soap: any,
+  Trees: any,
+  Wine: any,
+  Coffee: any,
+  Beans: any,
+  Fruits: any,
+  Grains: any,
+  Juices: any,
+  Mushrooms: any,
+  PetFood: any,
+  Tofu: any,
+  WildHarvested: any,
+  updateTime: any,
+}
+
+interface Cache<T> {
+  [key: number]: Array<T> 
+}
+
+const farmersMarketCache: Cache<FarmersMarketData> = {}
+async function getFarmersMarketDataCache(pagination: number, limit: number): Promise<FarmersMarketData[]>  {
+  if (farmersMarketCache[pagination]) {
+    return farmersMarketCache[pagination]
+  } else {
+    return await getFarmersMarketData(pagination, limit)
+  }
+}
+
+async function getFarmersMarketData (pagination: number, limit: number): Promise<FarmersMarketData[]> {
+  let base = 'SELECT * FROM farmers_markets_from_usda'
+  let limitQuery = ''
+  const data = []
+
+  if (pagination) {
+    const start = (pagination - 1) * limit
+    const end = pagination * limit
+    limitQuery = 'LIMIT ?, ?'
+    data.push(start, end)
+  }
+
+  const stmt = `${base} ORDER BY FMID ASC ${limitQuery}`
+  return await query(stmt, data)
+}
+
+function query (sql: string, params?: Array<any>): Promise<any[]> {
+  return new Promise((resolve, reject) => {
+    db.all(sql, params, (error, row) => {
+      if (error) {
+        return reject(error)
+      }
+      return resolve(row)
+    })
+  })
+}
+  
 
 app.listen(PORT, () => {
   console.log(`Opening app on port ${PORT}`)
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useMemo, useState, useEffect } from "react";
 import "./App.css";
 import * as superagent from "superagent";
-
-// export interface table {
-
-// }
+import Table from './Table'
 
 function App() {
   const [data, updateData] = useState([]);
   const [error, setError] = useState(false)
 
   useEffect(() => {
-    superagent.get("http://localhost:3000").end((error, res) => {
+    superagent.get("http://localhost:3001/FarmersMarket?pagination=1").end((error, res) => {
       if (error) {
         setError(true)
       }
@@ -19,15 +16,259 @@ function App() {
     });
   }, []);
 
+  
+
+  const columns = useMemo(() => [
+      {
+        Header: 'Data Table',
+        columns: [
+          {
+            Header: 'FMID',
+            accessor: 'FMID',
+          },
+          {
+            Header: 'Market Name',
+            accessor: 'MarketName',
+          },
+          {
+            Header: 'Website',
+            accessor: 'Website',
+          },
+          {
+            Header: 'Facebook',
+            accessor: 'Facebook',
+          },
+          {
+            Header: 'Twitter',
+            accessor: 'Twitter',
+          },
+          {
+            Header: 'Youtube',
+            accessor: 'Youtube',
+          },
+          {
+            Header: 'Other Media',
+            accessor: 'OtherMedia',
+          },
+          {
+            Header: 'Street',
+            accessor: 'street',
+          },
+          {
+            Header: 'County',
+            accessor: 'County',
+          },
+          {
+            Header: 'State',
+            accessor: 'State',
+          },
+          {
+            Header: 'ZIP Code',
+            accessor: 'zip'
+          },
+          {
+            Header: 'Season 1 Date',
+            accessor: 'Season1Date'
+          },
+          {
+            Header: 'Season 1 Time',
+            accessor: 'Season1Time'
+          },
+          {
+            Header: 'Season 2 Date',
+            accessor: 'Season2Date'
+          },
+          {
+            Header: 'Season 2 Time',
+            accessor: 'Season2Time'
+          },
+          {
+            Header: 'Season 3 Date',
+            accessor: 'Season3Date'
+          },
+          {
+            Header: 'Season 3 Time',
+            accessor: 'Season3Time'
+          },
+          {
+            Header: 'Season 4 Date',
+            accessor: 'Season4Date'
+          },
+          {
+            Header: 'Season 4 Time',
+            accessor: 'Season4Time'
+          },
+          {
+            Header: 'X',
+            accessor: 'x'
+          },
+          {
+            Header: 'Y',
+            accessor: 'y'
+          },
+          {
+            Header: 'Location',
+            accessor: 'Location'
+          },
+          {
+            Header: 'Credit',
+            accessor: 'Credit'
+          },
+          {
+            Header: 'WIC',
+            accessor: 'WIC'
+          },
+          {
+            Header: 'WIC Cash',
+            accessor: 'WICcash'
+          },
+          {
+            Header: 'SFMNP',
+            accessor: 'SFMNP'
+          }, 
+          {
+            Header: 'SNAP',
+            accessor: 'SNAP'
+          }, 
+          {
+            Header:'Organic',
+            accessor: 'Organic'
+          }, 
+          {
+            Header:'Baked Goods',
+            accessor: 'Bakedgoods'
+          }, 
+          {
+            Header:'Cheese',
+            accessor: 'Cheese'
+          }, 
+          {
+            Header:'Crafts',
+            accessor: 'Crafts'
+          }, 
+          {
+            Header:'Flowers',
+            accessor: 'Flowers'
+          }, 
+          {
+            Header:'Eggs',
+            accessor: 'Eggs'
+          }, 
+          {
+            Header:'Seafood',
+            accessor: 'Seafood'
+          }, 
+          {
+            Header:'Herbs',
+            accessor: 'Herbs'
+          }, 
+          {
+            Header:'Vegetables',
+            accessor: 'Vegetables'
+          }, 
+          {
+            Header:'Honey',
+            accessor: 'Honey'
+          }, 
+          {
+            Header:'Jams',
+            accessor: 'Jams'
+          }, 
+          {
+            Header:'Maple',
+            accessor: 'Maple'
+          }, 
+          {
+            Header:'Meat',
+            accessor: 'Meat'
+          }, 
+          {
+            Header:'Nursery',
+            accessor: 'Nursery'
+          }, 
+          {
+            Header:'Nuts',
+            accessor: 'Nuts'
+          }, 
+          {
+            Header:'Plants',
+            accessor: 'Plants'
+          }, 
+          {
+            Header:'Poultry',
+            accessor: 'Poultry'
+          }, 
+          {
+            Header:'Prepared',
+            accessor: 'Prepared'
+          }, 
+          {
+            Header:'Soap',
+            accessor: 'Soap'
+          }, 
+          {
+            Header:'Trees',
+            accessor: 'Trees'
+          }, 
+          {
+            Header:'Wine',
+            accessor: 'Wine'
+          }, 
+          {
+            Header:'Coffee',
+            accessor: 'Coffee'
+          }, 
+          {
+            Header:'Beans',
+            accessor: 'Beans'
+          }, 
+          {
+            Header:'Fruits',
+            accessor: 'Fruits'
+          }, 
+          {
+            Header:'Grains',
+            accessor: 'Grains'
+          }, 
+          {
+            Header:'Juices',
+            accessor: 'Juices'
+          }, 
+          {
+            Header:'Mushrooms',
+            accessor: 'Mushrooms'
+          }, 
+          {
+            Header:'PetFood',
+            accessor: 'PetFood'
+          }, 
+          {
+            Header:'Tofu',
+            accessor: 'Tofu'
+          }, 
+          {
+            Header:'WildHarvested',
+            accessor: 'WildHarvested'
+          }, 
+          {
+            Header:'updateTime',
+            accessor: 'updateTime'
+          }
+        ],
+      },
+    ],
+    []
+  )
+
   let table
-  if (data.length && !error) {
-    table = (
-      <div>
-        {Object.keys(data[0]).map(key => (
-          <span key={key}>{key}</span>
-        ))}
-      </div>
-    )
+  if (data.length) {
+    table = <Table columns={columns} data={data} />
+  } else {
+    if (error) {
+      table = <div>An Error Has Occured</div>
+    } else {
+      table = <div>Loading...</div>
+    }
   }
 
 
