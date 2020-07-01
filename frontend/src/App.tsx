121465423d136e86a216672d8d93dba09eb7798f
Myles Wardell <myles.wardell212@gmail.com>
2020-07-01 17:36:26 +1000

    Add 50 to limit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -358,6 +358,7 @@ function App () {
           <option value={5}>5</option>
           <option value={10}>10</option>
           <option value={20}>20</option>
+          <option value={50}>50</option>
           {/* <option value={0}>All</option> */}
         </select>
       </div>
